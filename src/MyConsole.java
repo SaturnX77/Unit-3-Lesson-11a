953a34dc1075c0acfa3815ec19a5117110626766
SaturnX77 <saturnvdt@gmail.com>
2022-11-11 18:35:06 -0800

    Initial commit

---
+++ src/MyConsole.java
@@ -0,0 +1,16 @@
+public class MyConsole {
+    public static void main(String[] args) {
+
+        // TO DO: Read the files you need into 1D arrays and create a Spotbox object with the arrays.
+        FileReader reader = new FileReader("src/artists");
+
+
+
+
+        // TO DO: Call the methods you write to provide the information to the user.
+
+
+
+
+    }
+}
---
+++ src/Spotbox.java
@@ -0,0 +1,30 @@
+/*
+ * Processes and analyzes data about music
+ */
+public class Spotbox {
+    String[] artists;
+    String[] genres;
+    String[] albums;
+
+    // TO DO: Declare instance variables for the arrays you need.
+    public Spotbox(String [] artists, String[] genres, String[] albums){
+        this.artists = artists;
+        this.genres = genres;
+        this.albums = artists;
+    }
+
+
+
+
+
+    // TO DO: Write a constructor to initialize the instance variables.
+
+
+
+    // TO DO: Write your methods to find the information you need about your data.
+
+
+
+
+
+}
